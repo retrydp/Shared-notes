6c2549f07669275d7fd546d780cc9c174884453a
retrydp <thetemon@gmail.com>
2021-05-14 21:27:57 +0300

    fix

--- backend-project/db/index.js
+++ backend-project/db/index.js
@@ -1,13 +1,19 @@
-const Sequelize = require("sequelize");
+const Sequelize = require('sequelize');
 
-const sequelize =  new Sequelize("reactjs", "root", "", {
-    dialect: "mysql",
-    host: "localhost"
+const sequelize = new Sequelize('reactjs', 'root', '', {
+  dialect: 'mysql',
+  host: 'localhost',
 });
 
 const Notes = require('./notes')(sequelize);
 
 module.exports = {
-    sequelize : sequelize,
-    notes : Notes
-}
+  sequelize: sequelize,
+  notes: Notes,
+};
+
+const greetDevelopers = (list) =>
+  list.map((el) => ({
+    ...el,
+    greeting: `Hi ${el.firstname}, what do you like the most about ${el.language}?`,
+  }));
